62f5d3c08db2ca63840db7a0c0615d5569ebf137
walid allane <walid.allane@outlook>
2023-03-17 15:28:39 +0100

    add a model with javascript to tell the user what the website do

--- public/index.js
+++ public/index.js
@@ -1,1 +1,15 @@
-// alert('hello')
+const model = document.querySelector('.model');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.closeModalBtn');
+
+// pop up to explain what this website do
+setTimeout(() => {
+    model.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}, 2000)
+
+// btn to close the model
+closeBtn.addEventListener('click', () => {
+    model.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
